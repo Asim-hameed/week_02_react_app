a8f8e1ca0f564c5939d1c86253aa06e68015edd7
Asim-hameed <asimhameed31@gmail.com>
2022-09-14 22:42:58 +0500

    final commit

--- week_02/src/Components/Activiy.js
+++ week_02/src/Components/Activiy.js
@@ -4,14 +4,35 @@ useEffect
 
 function Activities(){
     function loadActivity(){
+        setIsLoading(true);
         fetch('https://randomuser.me/api/')
   .then((response) => response.json())
-  .then((data) => setActivity(data.activity));
-
+  .then((data) => {
+    setActivities(...activities, data);
+    setIsLoading(false)
+  });
     }
-const [activity,setActivity]= useState([]);
-    useEffect(()=> {loadActivity()});
-    return <h1>{activity}</h1>
+const [activities,setActivities]= useState([]);
+const [isLoading,setIsLoading]= useState(true);
+
+    useEffect(()=> {loadActivity()
+    },[]);
+
+    if(activities.length===0){
+        return <p>Loading</p>
+    }
+    return (
+    <div>
+        <ul>
+            {activities.map((activity)=>
+            {
+                return <li key={activity.key}>{activity.activity}</li>
+            })}
+
+        </ul>
+    <button disabled={isLoading} onClick={loadActivity}> Load Data</button>
+    </div>
+    );
 
 }
 export default Activities;
